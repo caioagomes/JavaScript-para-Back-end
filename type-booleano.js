cbd325559d47307de69bf075d24fa846d34afe32
caioagomes <caio.gust@gmail.com>
2023-01-10 23:27:29 -0300

    exemplos da aula 1

---
+++ type-booleano.js
@@ -0,0 +1,11 @@
+//quando declaro a variavel como const, ela não poderá ter seu valor
+//alterado no futuro, por isso, se o valor for mudar é necessario utilizar
+// o let
+
+
+const primeiroNumero = 5;
+const segundoNumero = '5';
+let cadastroAtivado = true;
+
+console.log (primeiroNumero === segundoNumero)
+
---
+++ type-string.js
@@ -0,0 +1,14 @@
+const texto1 ="Olá, mundo!";
+const texto2 ='Olá, mundo!!!!';
+const senha = "senhaSuperSegura456!";
+const StringDeNumeros = "3456710";
+const meuNome= "Caio";
+const citacao="Meu nome é ";
+
+// concatenação (+)
+
+console.log(citacao+meuNome);
+
+
+
+//template string OU template literal
