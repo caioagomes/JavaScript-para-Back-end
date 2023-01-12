0d9dc8ab341a22c409ba6f16062f55267148681c
caioagomes <caio.gust@gmail.com>
2023-01-11 23:23:26 -0300

    conversão explicita e implicita

---
+++ conversoes.js
@@ -0,0 +1,19 @@
+// tipo de dado
+// booleanos
+
+//conversão implicita
+    //converter um tipo de dado em outro
+
+    const numero = 10;
+    const numeroString ="10";
+
+    //console.log(numero == numeroString);
+
+    ///console.log (numero + numeroString);
+
+//conversão explicita
+
+    //Number() função que converte string em numero
+    //String() função que converte numero em String
+
+    console.log(Number(numeroString)+numero);
