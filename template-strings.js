7f61cb53a970d2a01ec5066e72c4713c43876105
caioagomes <caio.gust@gmail.com>
2023-01-24 08:34:46 -0300

    adicionar variaveis em uma frase

--- template-strings.js
+++ template-strings.js
@@ -1,9 +1,9 @@
 const nome = "Caio";
-const idade = 26;
-const cidadeDeNascimento = "Belo Horitonte";
+const idade = 26; 
+const cidade= "Belo Horitonte";
 
 // const apresentacao = "meu nome é " + nome + ", minha idade é " + idade + " e nasci na cidade de " +cidadeDeNascimento ;
 // console.log(apresentacao)
 
-const apresentacao =  `meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeDeNascimento};` 
+const apresentacao =  `meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidade};` 
 console.log(apresentacao)
