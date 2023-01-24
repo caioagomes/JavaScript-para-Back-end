6cee6c0e22784bb358232ed37ec2cc7efe32fbd1
caioagomes <caio.gust@gmail.com>
2023-01-24 09:38:41 -0300

    funcoes 24.01.2023

---
+++ funcoes.js
@@ -0,0 +1,23 @@
+let x = "opa";
+let y= 547000;
+// console.log (x)
+// x = "opa";
+
+
+//primeiro momento: declara a função
+function imprimeTexto(texto){
+    console.log (texto)
+}
+
+//segundo momento: executa a função uma ou mais vezes
+
+imprimeTexto(x);
+imprimeTexto(y);
+
+//três formas de escrever funções
+
+function soma(){
+    const resultado = 2 +2;
+    return resultado
+}
+imprimeTexto(soma());
