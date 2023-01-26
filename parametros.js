66c10fd675cfc08bfd771e5f0f52a0c584b11732
caioagomes <caio.gust@gmail.com>
2023-01-25 23:17:16 -0300

    parametros

---
+++ parametros.js
@@ -0,0 +1,15 @@
+//sem parametros
+function soma1(){
+    return 2 + 2;
+}
+
+console.log (soma1());
+
+// com parametros
+let numero1=526668;
+let numero2=53;
+function soma(numero1,numero2){
+    return numero1 + numero2;
+}
+
+console.log (soma(numero1,numero2));
